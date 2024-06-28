501fbf9c702403cc558b3d7d3cf4bbb5600dfeda
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-06-28 11:53:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def generate_dataset(nbr):
         # scaling factor=1.3
         # Minimum neighbor = 5
  
-        if faces is ():
+        if faces == ():
             return None
         for (x, y, w, h) in faces:
             cropped_face = img[y:y + h, x:x + w]
